739c83c72ee5f61d2dc3d73487e1adf03b13eb6c
victor adenuga <victoradenuga04@yahoo.com>
2020-04-26 01:59:40 +0100

    channels can receive msgs

--- application.py
+++ application.py
@@ -2,14 +2,14 @@ import os
 import requests
 
 
-from flask import Flask, jsonify, render_template, request
+from flask import Flask, jsonify, render_template, request,redirect
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
 
-channels = {'talk': {'channel_name': 'talk', 'channel_creator': 'thor'},'talk2': {'channel_name': 'talk2', 'channel_creator': 'thor2'}}
+channels = [{'channel_name': 'talk', 'channel_creator': 'thor','messages':[{'message':'welcome', 'sender': 'vic','time': '10:11'},{'message':'welcome', 'sender': 'vic','time': '10:11'}] }, {'channel_name': 'talk2', 'channel_creator': 'thor2'}]
 
 @app.route("/")
 def index():
@@ -19,5 +19,29 @@ def index():
 def create_channel(data):
     new_channel = data['channel_name']
     display_name = data['display_name']
-    channels[new_channel] = {'channel_name': new_channel, 'channel_creator': display_name}
+    channels.append({'channel_name': new_channel, 'channel_creator': display_name})
     emit("channels", {'channel_name': new_channel, 'channel_creator': display_name}, broadcast=True)
+
+@app.route("/channel/<int:channel_id>")
+def channel(channel_id):
+	try:
+		channel_name = channels[channel_id]['channel_name']
+	except IndexError:
+		return	redirect("/")
+	messages = channels[channel_id]['messages']
+	return render_template("channel.html", channel_name=channel_name, channel_id=channel_id , messages=messages )
+
+# @app.route("/posts", methods=["POST"])
+# def posts():
+# 	data = channels[channel_id]['messages']
+# 	return jsonify(data)
+
+@socketio.on("add message")
+def add_message(data):
+    channel_id = data['channel_id']
+    del data['channel_id']
+    channels[channel_id]['messages'].append(data)
+    message = data['message']
+    sender = data['sender']
+    time = data['time']
+    emit("new message", {'message': message, 'sender': sender, 'time': time, 'channel_id': channel_id}, broadcast=True)
---
+++ static/channel.js
@@ -0,0 +1,75 @@
+
+document.addEventListener('DOMContentLoaded', () =>{
+	const display_name = localStorage.getItem('display_name');
+    // Connect to websocket
+    var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
+    // When connected, configure buttons
+    socket.on('connect', () => {
+    	// By default, submit button is disabled
+    	document.querySelector('#submit').disabled = true;
+    	// Enable button only if there is text in the input field
+    	document.querySelector('#task').onkeyup = () => {
+        	if (document.querySelector('#task').value.length > 0)
+            	document.querySelector('#submit').disabled = false;
+        	else
+            	document.querySelector('#submit').disabled = true;
+    	};
+
+    	document.querySelector('#new_message').onsubmit = () => {
+    		var newDate = new Date();
+			dateString = newDate.toUTCString();
+
+        	const new_message = document.querySelector('#task').value;
+        	socket.emit('add message', {'message': new_message, 'sender': display_name,'time': dateString, 'channel_id': channel_id});
+
+            // Clear input field and disable button again
+        	document.querySelector('#task').value = '';
+        	document.querySelector('#submit').disabled = true;
+       		// Stop form from submitting
+        	return false;
+   		 };
+    }); 
+
+        // When a new vote is announced, add to the unordered list
+    socket.on('new message', data => {
+
+                    // Create new item for list
+        const a = document.createElement('div');
+        const b = document.createElement('div');
+        const c = document.createElement('blockquote');
+        const d = document.createElement('p');
+        const e = document.createElement('footer');
+        const f = document.createElement('cite');
+        a.className = "card";
+        if (data.sender === display_name)
+        	a.setAttribute("id", "chat_box1");
+        else
+        	a.setAttribute("id", "chat_box2");
+        b.setAttribute("class", "card-body");
+        c.setAttribute("class", "blockquote mb-0");
+        e.setAttribute("class", "blockquote-footer");
+        f.setAttribute("title", "Source Title");
+        d.innerHTML = data.message;
+        e.innerHTML = `${data.sender} @`;
+        f.innerHTML = data.time;
+        e.innerHTML += f.outerHTML;
+        c.innerHTML = d.outerHTML + e.outerHTML
+        b.innerHTML = c.outerHTML;
+        a.innerHTML = b.outerHTML;
+
+                // Add new item to task list
+        document.querySelector('#message').append(a);
+        
+    });
+
+});
+
+
+// <div class="card" id="chat_box1">
+// 	<div class="card-body">
+// 	<blockquote class="blockquote mb-0">
+// 				<p>{{message['message']}}</p>
+// 				<footer class="blockquote-footer">{{message['sender']}} @<cite title="Source Title">{{message['time']}}</cite></footer>
+// 	</blockquote>
+// 	</div>
+// </div>
