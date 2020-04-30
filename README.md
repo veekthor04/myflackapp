8ea7aeee304e2fb94c3267b6bb1ed5cac917cb5b
veekthor04 <54349080+veekthor04@users.noreply.github.com>
2020-04-30 14:06:37 +0100

    final

--- README.md
+++ README.md
@@ -1,3 +1,24 @@
 # Project 2
 
 Web Programming with Python and JavaScript
+
+My Flack app is an online messaging service using flask where users can sign 
+in with a display name, create channels as in slack, see existing channels and
+join channels. When a channel is selected, user will be able to send,receive and
+delete their own messages in real time.
+file info:
+application.py: is the main app file. Contains routing to all html files ,ajax 
+and socket broadcast
+
+index.html: is the page which allows users to sign in with a display name,
+create channels and join desirable channel
+channel.html: is the page which allows user to send and receive messages within
+the selected channel
+
+index.css: this is the style sheet for index.html
+
+channel.css: this is the style sheet for channel.html
+
+index.js: this contains the javascript commands for index.html
+
+channel.js this contains the javascript commands for channel.html
--- application.py
+++ application.py
@@ -9,39 +9,65 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
 
-channels = [{'channel_name': 'talk', 'channel_creator': 'thor','messages':[{'message':'welcome', 'sender': 'vic','time': '10:11'},{'message':'welcome', 'sender': 'vic','time': '10:11'}] }, {'channel_name': 'talk2', 'channel_creator': 'thor2','messages':[{'message':'welcome', 'sender': 'vic','time': '10:11'},{'message':'welcome', 'sender': 'vic','time': '10:11'}]}]
-
+channels = []
+max_msg = 100
 @app.route("/")
 def index():
+	# renders homepage
     return render_template("index.html", channels=channels)
 
 @socketio.on("create channel")
 def create_channel(data):
+	# creates new channel
     new_channel = data['channel_name']
     display_name = data['display_name']
+    channel_id = len(channels)
     channels.append({'channel_name': new_channel, 'channel_creator': display_name})
-    emit("channels", {'channel_name': new_channel, 'channel_creator': display_name}, broadcast=True)
+    channels[channel_id]['messages'] = []
+    #emits the channel created
+    emit("channels", {'channel_name': new_channel, 'channel_creator': display_name, 'channel_id': channel_id}, broadcast=True)
 
 @app.route("/channel/<int:channel_id>")
 def channel(channel_id):
+	# open channel page for chat
 	try:
 		channel_name = channels[channel_id]['channel_name']
 	except IndexError:
 		return	redirect("/")
-	messages = channels[channel_id]['messages']
-	return render_template("channel.html", channel_name=channel_name, channel_id=channel_id , messages=messages )
-
-# @app.route("/posts", methods=["POST"])
-# def posts():
-# 	data = channels[channel_id]['messages']
-# 	return jsonify(data)
+	return render_template("channel.html", channel_name=channel_name, channel_id=channel_id )
 
 @socketio.on("add message")
 def add_message(data):
+	# adds new message to the channel 
     channel_id = data['channel_id']
     del data['channel_id']
+    msg_length = len(channels[channel_id]['messages'])
+    if len(channels[channel_id]['messages']) >= max_msg:
+        channels[channel_id]['messages'].pop(0)
     channels[channel_id]['messages'].append(data)
-    message = data['message']
+    message = data['message'].strip()
     sender = data['sender']
-    time = data['time']
-    emit("new message", {'message': message, 'sender': sender, 'time': time, 'channel_id': channel_id}, broadcast=True)
+    time = data['time'].strip()
+    #emits the message added
+    emit("new message", {'message': message, 'sender': sender, 'time': time,'msg_length': msg_length, 'channel_id': channel_id}, broadcast=True)
+
+@app.route("/posts", methods=["POST"])
+def posts():
+
+    # get axisting messages for the channel
+    channel_id = int(request.form.get("channel_id"))
+    return jsonify(channels[channel_id]['messages'])
+
+@app.route("/delete", methods = ["POST"])
+def delete():
+	# deletes the message from the channel 
+    channel_id = int(request.form.get("channel_id"))
+    message = request.form.get("message").strip()
+    time = request.form.get("time").strip()
+    sender  = request.form.get("sender")
+   	# checks for the message
+    for index in range(len(channels[channel_id]['messages'])):
+    	if(channels[channel_id]['messages'][index]['sender'] == sender and channels[channel_id]['messages'][index]['time'] == time ):
+        	del channels[channel_id]['messages'][index]
+
+    return '', 204
